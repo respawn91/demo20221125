9e7a665420cc0d0df4962928ae58e0f6a39f86df
Anton Tarasov <respawn91@gmail.com>
2022-11-25 19:38:23 +0300

    Create employee.java

---
+++ employee.java
@@ -0,0 +1,38 @@
+public class Employee extends Person {
+    
+    public int salary;
+    public int experience;
+    public boolean isEducated;
+    public boolean isHappy;
+    public Laptop device;
+    public Manager mentor;
+    
+    public Employee(String name, int age, String sex) {
+        super(name, age, sex);
+    }
+
+    public Employee(String name, int age, String sex, int salary, int experience, boolean isEducated) {
+        super(name, age, sex);
+        this.salary = salary;
+        this.experience = experience;
+        this.isEducated = isEducated;
+        
+        if(sex == "female") {
+            this.isHappy = true;
+        }
+        else {
+            if(salary > 50000) {
+                this.isHappy = true;
+            }
+            else {
+                this.isHappy = false;
+            }
+        }
+    }
+    
+    public void print() {
+        System.out.printf("Hello! My name is %s. I am %d y.o. %s employee\n", this.name, this.age, this.sex);
+        System.out.printf("Also, my salary is %d, my experience is %d. Education? %b. Happy? %b\n", this.salary, this.experience, this.isEducated, this.isHappy);
+    }
+    
+}
